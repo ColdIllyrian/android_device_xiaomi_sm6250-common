1df75b4fb635a9dd25429b207c9695356de97bb2
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2020-06-05 00:16:06 +0200

    sm6250-common: update extract scripts for curtana

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -7,7 +7,7 @@
 
 set -e
 
-INITIAL_COPYRIGHT_YEAR=2019
+INITIAL_COPYRIGHT_YEAR=2020
 
 # Load extract_utils and do some sanity checks
 MY_DIR="${BASH_SOURCE%/*}"
@@ -26,7 +26,7 @@ source "${HELPER}"
 setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${LINEAGE_ROOT}" true
 
 # Copyright headers and guards
-write_headers "davinci phoenix violet"
+write_headers "curtana"
 
 # The standard common blobs
 write_makefiles "${MY_DIR}/proprietary-files.txt" true
